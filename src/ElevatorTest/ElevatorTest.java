1402e086d017c4718376ce4922efee6f5613c992
Anant Ojha <anantojha@cmail.carleton.ca>
2022-02-02 02:37:12 -0500

    Fixed test csv folder error

--- src/ElevatorTest/ElevatorTest.java
+++ src/ElevatorTest/ElevatorTest.java
@@ -1,6 +1,5 @@
 package ElevatorTest;
 
-import Elevator.Main;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,11 +13,28 @@ public class ElevatorTest {
     // Test input csv generation
     @Test
     public void testInputCsvGeneration() throws IOException {
-        Main.createFloorCSV(1, "TestFloorCSV");
+        File TestFolder = new File("CSV/TestFloorCSV");
+
+        // create test csv folder if one doesn't exist
+        if(!TestFolder.exists()){
+            TestFolder.createNewFile();
+        } else {
+            // if folder exists, delete all files from folder
+            for(File f: TestFolder.listFiles())
+                if (!f.isDirectory())
+                    f.delete();
+        }
+
+        // generate test csv
+        Elevator.Main.createFloorCSV(1, "TestFloorCSV");
         File file = new File("CSV/TestFloorCSV/floor_1.csv");
+
+        // assert file exists and is not a directory
         assertTrue(file.exists());
         assertFalse(file.isDirectory());
-        file.delete();
+
+        // delete after when test is complete
+        // file.delete();
     }
 
 

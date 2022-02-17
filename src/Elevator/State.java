9bbfafc07af153eb7207c279b85b27c9f1242bb5
Navaty Khara <navatykhara@gmail.com>
2022-02-17 00:30:49 -0500

    State

---
+++ src/Elevator/State.java
@@ -0,0 +1,11 @@
+package Elevator;
+/*
+ * State is a collection of constants representing the different states the state machine can take 
+ * 
+ * 
+ */
+public enum State {
+	INITIALIZE,
+	RUNNING,
+	CLOSING
+}

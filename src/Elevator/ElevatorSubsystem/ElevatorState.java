88af1bdb76073d33824e293e8dd05205789ce65b
Anant Ojha <anantojha@cmail.carleton.ca>
2022-03-27 21:49:02 -0400

    elevator state fix

--- src/Elevator/ElevatorSubsystem/ElevatorState.java
+++ src/Elevator/ElevatorSubsystem/ElevatorState.java
@@ -2,6 +2,8 @@ package Elevator.ElevatorSubsystem;
 
 import Elevator.Enums.Direction;
 import Elevator.Enums.ElevatorStatus;
+import Elevator.Enums.FloorStatus;
+import Elevator.Global.StateMachine;
 import Elevator.Global.SystemConfiguration;
 
 import java.util.Observable;
@@ -10,101 +12,39 @@ import java.util.Observable;
  * ElevatorState represents the State class of the Elevator
  * 
  */
-public class ElevatorState {
+public class ElevatorState implements StateMachine {
 
-    private Integer currentFloor;
-    private Direction direction;
-    private ElevatorStatus status;
-    private Integer maxFloor;
 
-	/*
-	 * ElevatorState(Integer) is the constructor of the ElevatorState class. It initializes all necessary variables.
-	 * 
-	 * Input: Integer startFloor
-	 * Output: none
-	 * 
-	 */
-    public ElevatorState(Integer startFloor){
-        this.currentFloor = startFloor;
-        this.direction = Direction.UP;
-        this.status = ElevatorStatus.IDLE;
-        this.maxFloor = SystemConfiguration.MAX_FLOOR;
-    }
+	private ElevatorContext state;
+	private ElevatorContext previousState;
+	private ElevatorContext nextState;
 
-	/*
-	 * getCurrentFloor() gets the current floor.
-	 * 
-	 * Input: none
-	 * Output: Integer currentFloor
-	 * 
-	 */
-    public Integer getCurrentFloor() {
-        return currentFloor;
-    }
+	public ElevatorState(ElevatorContext initialContext) {
+		this.state = initialContext;
+		this.previousState = null;
+		this.nextState = null;
+	}
 
-	/*
-	 * setCurrentFloor(Integer) sets the current floor equal to the parameter
-	 * 
-	 * Input: Integer currentFloor
-	 * Output: none
-	 * 
-	 */
-    public void setCurrentFloor(Integer currentFloor) {
-        this.currentFloor = currentFloor;
-    }
+	public ElevatorState setElevatorContext(ElevatorContext context){
+		nextState = context;
+		return this;
+	}
 
-	/*
-	 * getDirection() gets the current direction.
-	 * 
-	 * Input: none
-	 * Output: Direction direction
-	 * 
-	 */
-    public Direction getDirection() {
-        return direction;
-    }
+	@Override
+	public void updateState() {
+		ElevatorContext temp  = state;
+		this.state = nextState;
+		previousState = temp;
+		// System.out.println(state.toString());
+	}
 
-	/*
-	 * setDirection() sets the current direction equal to the parameter.
-	 * 
-	 * Input: Direction direction
-	 * Output: none
-	 * 
-	 */
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
+	@Override
+	public String getCurrentState() {
+		return state.toString();
+	}
 
-	/*
-	 * getStatus() gets the maximum floor.
-	 * 
-	 * Input: none
-	 * Output: ElevatorStatus status
-	 * 
-	 */
-    public ElevatorStatus getStatus() {
-        return status;
-    }
-
-    /*
-	 * setStatus(ElevatorMachineStatus) sets the current state equal to paramater.
-	 * 
-	 * Input: ElevatorMachineStatus state
-	 * Output: none
-	 * 
-	 */
-    public void setStatus(ElevatorStatus status) {
-        this.status = status;
-    }
-
-	/*
-	 * getMaxFloor() gets the maximum floor.
-	 * 
-	 * Input: none
-	 * Output: Integer
-	 * 
-	 */
-    public Integer getMaxFloor() {
-        return maxFloor;
-    }
+	@Override
+	public String getPreviousState() {
+		return previousState.toString();
+	}
 }
